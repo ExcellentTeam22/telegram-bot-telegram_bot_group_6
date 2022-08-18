5ebf18e60f4650a223b2c322e391cc53dbf7d8ee
Eliad Karni <EliadK995@Gmail.com>
2022-08-18 13:49:40 +0300

    Math bot and splitted to files

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from analyser import RESPONSES
 app = Flask(__name__)
 
 TOKEN = "5458454629:AAHTr7Am8RJ71v2MsIxaviJHoGbB5OO7ucg"
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=https://d9cf-82-80-173-170.eu.ngrok.io/message'.format(TOKEN)
+TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=https://d9cf-82-80-173-170.eu.ngrok.io/message'\
+    .format(TOKEN)
 
 MESSAGE =""" Message """
 
@@ -21,7 +22,8 @@ def handle_message():
     print("got a message")
     message = request.get_json()
     chat_id = message['message']['chat']['id']
-    res = requests.get('''https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'''.format(TOKEN, chat_id, analyse_message(message["message"]["text"])))
+    res = requests.get('''https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'''
+                       .format(TOKEN, chat_id, analyse_message(message["message"]["text"])))
     return Response("message returned")
 
 
